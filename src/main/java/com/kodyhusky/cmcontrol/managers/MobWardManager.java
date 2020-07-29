643ca6e54793031e6d55e3cd48797c1f8c276700
Jeffery Hancock <jefferyphancock@outlook.com>
2020-07-29 01:32:00 -0400

    Fix Coming

--- src/main/java/com/kodyhusky/cmcontrol/managers/MobWardManager.java
+++ src/main/java/com/kodyhusky/cmcontrol/managers/MobWardManager.java
@@ -2,6 +2,7 @@ package com.kodyhusky.cmcontrol.managers;
 
 import com.kodyhusky.cmcontrol.CompleteMobControl;
 import com.kodyhusky.cmcontrol.objects.MobWard;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -22,41 +23,59 @@ public class MobWardManager {
     private HashMap<UUID,Pair<Location,Integer>> activemw;
 
     public MobWardManager(CompleteMobControl plugin) {
+        mobwards = new HashMap<>();
+        activemw = new HashMap<>();
         this.plugin = plugin;
     }
     
+    // =========================================================================
+    // TEMPORARY TESTING FUNCTION (world,0,80,0) default
+    // =========================================================================
     public void addDebugMobWard() {
-        mobwards = new HashMap<>();
-        activemw = new HashMap<>();
-        UUID newuuid = UUID.randomUUID();
+        UUID uuid = UUID.randomUUID();
+        UUID puuid = plugin.getServer().getPlayer("klimaxthefox").getUniqueId();
         World world = plugin.getServer().getWorld("world");
-        MobWard debugward = new MobWard(
-                "debug",newuuid,"",
-                (new Location(world,0,64,0)),
-                (new Location(world,0,64,0)),
-                UUID.randomUUID(),
-                null,null,true);
-        mobwards.put(newuuid, debugward);
+        Location noloc = new Location(world,0,80,0);
+        MobWard debugward = new MobWard("debug",uuid,"AIR",noloc,noloc,puuid,
+                (new ArrayList<>()),(new ArrayList<>()),(new ArrayList<>()),true);
+        mobwards.put(uuid, debugward);
         @SuppressWarnings("unchecked")
-        Pair<Location,Integer> pair = (new Pair(new Location(world,0,64,0), 10));
-        activemw.put(newuuid, pair);
+        Pair<Location,Integer> pair = (new Pair<>(noloc, 16));
+        activemw.put(uuid, pair);
+    }
+    // =========================================================================
+    
+    public boolean isEntityAllowed(Entity entity) {
+        return false;
     }
 
-    /**
-     * Checks if an entity is blocked by an active MobWard.
-     * 
-     * @param entity A Entity to check
-     * @return Boolean If the Entity spawn is allowed.
-     */
-    @SuppressWarnings("unchecked")
-    public boolean isMWSpawnAllowed(Entity entity) {
-        for (Map.Entry<UUID,Pair<Location,Integer>> entry : activemw.entrySet()) {
-            UUID mward = entry.getKey();
-            Location mwloc = entry.getValue().getKey();
-            Integer radius = entry.getValue().getValue();
-            if (entity.getLocation().distanceSquared(mwloc) <= radius * radius) {
-                MobWard mwards = mobwards.get(mward);
-                return mwards.isSpawnAllowed(entity);
+    public boolean isSpawnBlocked(Entity entity) {
+        
+        int rtype = 1;
+        
+        
+        // Cube Type
+        if (rtype == 0) {
+            
+            
+            
+        }
+        // Sphere Radius Type
+        if (rtype == 1) {
+            for (Map.Entry<UUID,Pair<Location,Integer>> entry : activemw.entrySet()) {
+                Location mwloc = entry.getValue().getKey();
+                int radius = entry.getValue().getValue();
+                if (entity.getLocation().distanceSquared(mwloc) <= radius * radius) {
+                    MobWard ward = mobwards.get(entry.getKey());
+                    if ((ward.hasOption("SEARCH_BELOW") && entity.getLocation().getY() < mwloc.getY()) ||
+                            (ward.hasOption("SEARCH_ABOVE") && entity.getLocation().getY() >= mwloc.getY())) {
+                        if (ward.hasOption("BLACKLIST") || ward.hasOption("WHITELIST")) {
+                            return ward.isEntityAllowed(entity);
+                        } else {
+                            return isEntityAllowed(entity);
+                        }
+                    }
+                }
             }
         }
         return false;
--- src/main/java/com/kodyhusky/cmcontrol/objects/MobWard.java
+++ src/main/java/com/kodyhusky/cmcontrol/objects/MobWard.java
@@ -8,13 +8,15 @@ package com.kodyhusky.cmcontrol.objects;
 import java.util.ArrayList;
 import java.util.UUID;
 import org.bukkit.Location;
-import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Monster;
 
 /**
+ * An object containing all data for a given MobWard.
+ * <br>Note1: Base Location, UUID, and Type cannot be
+ * changed once the object is constructed.
  *
- * @author Kody
+ * @author xkodyhuskyx
+ * @version 3.0.0
  */
 public class MobWard {
     
@@ -26,11 +28,27 @@ public class MobWard {
     private UUID owner;
     private ArrayList<UUID> managers;
     private ArrayList<String> options;
-    private Boolean debug;
+    private ArrayList<String> customlist;
+    private boolean debug;
 
+    /**
+     * Creates an object containing all data for a given MobWard.
+     * <br>Note1: Base Location, UUID, and Type cannot be
+     * changed once the object is constructed.
+     * @param name Given Name
+     * @param uuid Unique Identifier
+     * @param type Material
+     * @param baseblock Base Location
+     * @param powerblock Power block Location
+     * @param owner Owners UUID
+     * @param managers List of managers UUID's
+     * @param options Options
+     * @param customlist Custom entity list
+     * @param debug Debug mode
+     */
     public MobWard(String name, UUID uuid, String type, Location baseblock,
             Location powerblock, UUID owner, ArrayList<UUID> managers,
-            ArrayList<String> options, Boolean debug) {
+            ArrayList<String> options, ArrayList<String> customlist, boolean debug) {
         this.name = name;
         this.uuid = UUID.randomUUID();
         this.type = type;
@@ -42,9 +60,72 @@ public class MobWard {
         this.debug = debug;
     }
     
-    public Boolean isSpawnAllowed(Entity entity) {
-        return false;
+    /**
+     * Get the current blacklist / whitelist mode.<br>
+     * <br>0 - Default (Don't Use White/Blacklist)
+     * <br>1 - Blacklist (Deny Specific Mobs)
+     * <br>2 - Whitelist (Allow Specific Mobs)
+     * @return int - list type
+     */
+    public int entityListType() {
+        if (options.contains("BLACKLIST")) {return 1;}
+        if (options.contains("WHITELIST")) {return 2;}
+        return 0;
     }
+    
+    /**
+     * Gets if the specified option has been set.
+     * @param option Option to check
+     * @return boolean - has option
+     */
+    public boolean hasOption(String option) {
+        return this.options.contains(option);
+    }
+    
+    /**
+     * Get if the specified entity spawn should be allowed.
+     * <br>!!Do not use with entity list type 0 (Default)!!
+     * @param entity entity trying to spawn
+     * @return Boolean - spawn allowed
+     */
+    public boolean isEntityAllowed(Entity entity) {
+        if (options.contains("BLACKLIST")) {
+            return !customlist.contains(entity.getType().toString().toUpperCase());
+        }
+        if (options.contains("WHITELIST")) {
+            return customlist.contains(entity.getType().toString().toUpperCase());
+        }
+        return true;
+    }
+    
+    /**
+     * Assign a custom entity list.
+     * <br>Ensure all values are in uppercase!
+     * <br>Assign empty ArrayList to clear.
+     * @param customlist list of entity names
+     */
+    public void setCustomEntityList(ArrayList<String> customlist) {
+        this.customlist = new ArrayList<>();
+        if (!customlist.isEmpty()) {this.customlist.addAll(customlist);}
+    }
+    
+    /**
+     * For development purposes only! May spam the console and global chat!
+     * @return Boolean - debug mode enabled
+     */
+    public boolean isDebugEnabled() {
+        return this.debug;
+    }
+    
+    /**
+     * For development purposes only! May spam the console and global chat!
+     * @param debug debug mode
+     */
+    public void setDebugMode(Boolean debug) {
+        this.debug = debug;
+    }
+    
+    
     
     
     
